b621df3e3bc566e6c22e7c2691a4889473607628
Hossam <hossamczl1522@gmail.com>
2024-05-06 17:31:38 +0200

    Practica empezada

---
+++ practica.py
@@ -0,0 +1,117 @@
+import cv2
+import numpy as np
+import cuia
+from matplotlib import pyplot as plt
+import pyrender
+import trimesh
+import camara
+import mathutils
+import math
+
+# AVISO; Las coordenadas para referirse a los píxeles de una imagen son (columna, fila)
+
+# Función para invertir las componentes Y y Z del opencv original para que no hay problmeas con pyrender
+def fromOpencvToPyrender(rvec, tvec):
+  pose = np.eye(4)
+  pose[0:3,3] = tvec.T
+  pose[0:3,0:3] = cv2.Rodrigues(rvec)[0]
+  pose[[1,2]] *= -1
+  pose = np.linalg.inv(pose)
+  return(pose)
+
+escena = pyrender.Scene(bg_color=(0,0,0), ambient_light=(1.0, 1.0, 1.0))
+
+
+
+# Modelo 3D
+nombrefi = "media/cubo.glb"
+modelo_trimesh = trimesh.load(nombrefi, file_type='glb')
+modelo_mesh = pyrender.Mesh.from_trimesh(list(modelo_trimesh.geometry.values()))
+
+mat_loc = mathutils.Matrix.Translation((0.0, 0.0, 0.05))
+
+mat_rot = mathutils.Matrix.Rotation(math.radians(90.0), 4, 'X')
+
+mat_sca = mathutils.Matrix.Scale(1.0, 4)
+
+meshpose = mat_loc @ mat_rot @ mat_sca
+
+modelo = pyrender.Node(mesh=modelo_mesh, matrix=meshpose) # Creamos un nodo indicando la malla y su pose
+escena.add_node(modelo) # Y la añadimos a la escena
+
+
+
+fx = camara.cameraMatrix[0][0]
+fy = camara.cameraMatrix[1][1]
+cx = camara.cameraMatrix[0][2]
+cy = camara.cameraMatrix[1][2]
+
+camInt = pyrender.IntrinsicsCamera(fx, fy, cx, cy)
+cam = pyrender.Node(camera=camInt)
+escena.add_node(cam)
+
+
+
+camId = 0
+bk = cuia.bestBackend(camId)
+ar = cuia.myVideo(camId, bk)
+hframe = ar.get(cv2.CAP_PROP_FRAME_HEIGHT)
+wframe = ar.get(cv2.CAP_PROP_FRAME_WIDTH)
+mirender = pyrender.OffscreenRenderer(wframe, hframe)
+
+
+
+diccionario = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_5X5_50)
+detector = cv2.aruco.ArucoDetector(diccionario)
+
+def detectarPose(frame, idMarcador, tam):
+    bboxs, ids, rechazados = detector.detectMarkers(frame)
+    if ids is not None:
+        for i in range(len(ids)):
+            if ids[i] == idMarcador:
+                objPoints = np.array([[-tam/2.0, tam/2.0, 0.0],
+                                      [tam/2.0, tam/2.0, 0.0],
+                                      [tam/2.0, -tam/2.0, 0.0],
+                                      [-tam/2.0, -tam/2.0, 0.0]])
+                ret, rvec, tvec = cv2.solvePnP(objPoints, bboxs[i], camara.cameraMatrix, camara.distCoeffs)
+                if ret:
+                    return((True, rvec, tvec))
+    return((False, None, None))
+
+def realidadMixta(renderizador, frame, escena):
+    color, m = renderizador.render(escena)
+    bgr = cv2.cvtColor(color, cv2.COLOR_RGB2BGR) #convertimos la imagen de color al espacio BGR
+
+    _, m = cv2.threshold(m, 0, 1, cv2.THRESH_BINARY) #Umbralizamos la matriz de profundidad poniendo a cero los valores negativos y el resto a uno
+    m = (m*255).astype(np.uint8) #Para usarla como canal alfa necesitamos expresarla en el rango [0,255] como números enteros
+    m = np.stack((m,m,m), axis=2) #Creamos una imagen de 3 bandas repitiendo la máscara obtenida
+
+    #A continuación empleamos la máscara y su inversa para combinar la imagen del frame con la imagen generada por el render
+    inversa = cv2.bitwise_not(m)
+    pp = cv2.bitwise_and(bgr, m)
+    fondo = cv2.bitwise_and(frame, inversa)
+    res = cv2.bitwise_or(fondo, pp)
+    return(res)
+
+# Función para mostrar modelos 3D en la cámara
+def mostrarModelo(frame):
+    ret, rvec, tvec = detectarPose(frame, 0, 0.19) #Buscaremos el marcador 0 impreso con 19cm de lado
+    if ret:
+        poseCamara = fromOpencvToPyrender(rvec, tvec) #Determinamos la posición de la cámara en forma de matriz de transformación de Pyrender
+        escena.set_pose(cam, poseCamara) #Ubicamos la cámara en la posición obtenido
+        frame = realidadMixta(mirender, frame, escena) #Mezclamos mundo real y mundo virtual
+    return(frame)
+
+
+
+# Mostrar el modelo 3D encima de un marcador de la bilbioteca
+ar.process = mostrarModelo
+try:
+    ar.play("AR", key=ord(' '))
+finally:
+    ar.release()
+
+
+
+
+# Idea -> Con una biblioteca de marcadores leer y según lo que se lea se procesa un modelo 3D
